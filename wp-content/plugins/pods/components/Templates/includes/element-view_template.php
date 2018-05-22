175413bdf3675c38cdd0c28c10f695fdb1151613
tomassams <31186481+tomassams@users.noreply.github.com>
2018-05-22 11:43:34 +0200

    Initialize wordpress

---
+++ wp-content/plugins/pods/components/Templates/includes/element-view_template.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Frontier Template code editor metabox
+ */
+?><textarea id="content" name="content">
+<?php
+if ( isset( $content ) ) {
+	echo esc_textarea( $content );
+}
+?>
+	</textarea>
