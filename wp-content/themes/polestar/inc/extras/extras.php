79b922827aceb8cdfdea6f4d430ff100903d2906
tomassams <31186481+tomassams@users.noreply.github.com>
2018-05-29 10:12:00 +0200

    delete scrapped theme

--- wp-content/themes/polestar/inc/extras/extras.php
+++
@@ -1,7 +0,0 @@
-<?php	
-
-/**
- * Load all the extra components.
- */
-require get_template_directory() . '/inc/extras/about-page.php';
-require get_template_directory() . '/inc/extras/page-settings.php';
